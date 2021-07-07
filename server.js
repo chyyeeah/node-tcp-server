6b729ed305dbf6233924df444d4adb1690b6a164
Wilson Cheah <wilsoncheah5@gmail.com>
2021-07-06 23:24:18 -0700

    added functionality to return fake data and pass in a custom port number

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const net = require('net');
+const faker = require('faker');
 
 const server = net.createServer(socket => {
   console.log('client connected');
   socket.on('data', (data) => {
-    socket.write('hello!\n')
+    if (data.toString().trim() === 'uuid') {
+      socket.write(faker.fake('{{datatype.uuid}}\n'))
+    }
+    if (data.toString().trim() === 'first name') {
+      socket.write(faker.fake('{{name.firstName}}\n'))
+    }
+    if (data.toString().trim() === 'last name') {
+      socket.write(faker.fake('{{name.lastName}}\n'))
+    }
+    if (data.toString().trim() === 'full name') {
+      socket.write(faker.fake('{{name.firstName}} {{name.lastName}}\n'))
+    }
+    if (data.toString().trim() === 'coin flip') {
+      const result = faker.fake('{{datatype.boolean}}');
+      if (result === 'true') {
+        socket.write('heads\n');
+      } else {
+        socket.write('tails\n');
+      }
+    }
   });
 });
 
-server.listen(3000, () => console.log('listening on port 3000'));
+const PORT = process.argv[2];
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
